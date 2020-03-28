2c638fbb87991b925a575c64e39c1fe8494a0c01
mauricio ivan orta nuñez <zmaut@hotmail.com>
2020-03-28 13:21:04 -0600

    End of section 3

---
+++ client-app/src/App.tsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios';
+import { Header, Icon, List } from 'semantic-ui-react'
+
+class App extends Component {
+state = {
+  values: []
+}
+
+componentDidMount() {
+  axios.get('http://localhost:5001/api/values')
+  .then((response) => {
+   // console.log(response);
+    this.setState({
+      values: response.data
+    })
+  })
+  
+}
+
+ render () {
+  return (
+    <div>
+      <Header as='h2' icon>
+        <Icon name='users' />
+          Reactivities
+        <Header.Subheader>
+          Manage your account settings and set e-mail preferences.
+        </Header.Subheader>
+      </Header>
+      <List>
+          {this.state.values.map((value: any) => (
+            <List.Item key={value.id}>{value.name}</List.Item>
+          ))}
+
+      </List>
+    </div>
+  );
+ } 
+}
+
+export default App;
