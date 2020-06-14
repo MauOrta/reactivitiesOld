4ab0543bd5e6fe2bb4eae2797bfa3871d35577a5
mauricio ivan orta nuñez <zmaut@hotmail.com>
2020-06-14 12:39:34 -0500

    End of Section 6

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -8,13 +8,15 @@ interface IProps {
     activity: IActivity;
     createActivity: (activity: IActivity) => void;
     editActivity: (activity: IActivity) => void;
+    submitting: boolean
 }
 
 export const ActivityForm: React.FC<IProps> = ({
     setEditMode, 
     activity: initialFormState,
     editActivity,
-    createActivity}) => {
+    createActivity,
+    submitting}) => {
     const initializeForm = () => {
         if (initialFormState) {
             return initialFormState
@@ -59,7 +61,7 @@ export const ActivityForm: React.FC<IProps> = ({
                 <Form.Input onChange={handeInputChange} name='date' type='datetime-local' placeholder='Date' value={activity.date}/>
                 <Form.Input onChange={handeInputChange} name='city' placeholder='City' value={activity.city} />
                 <Form.Input onChange={handeInputChange} name='venue' placeholder='Venue' value={activity.venue}/>
-                <Button  floated='right' positive type='submit' content='Submit' />
+                <Button loading={submitting} floated='right' positive type='submit' content='Submit' />
                 <Button onClick={() => setEditMode(false)} floated='right' type='button' content='Cancel' />
             </Form>
         </Segment>
